fe2df7486e8e4576290ff46b7609d1849145585e
Waldemar Thiel <waldemar.thiel@oracle.com>
2018-02-14 13:10:37 +0100

    enable mysql

--- routes/creditscore.js
+++ routes/creditscore.js
@@ -25,7 +25,6 @@ exports.list = function(req, res){
 //    res.setHeader('Content-Type', 'application/json');
 //    res.send(JSON.stringify(resultData));
 
-
     // Start of db code block
     console.log("Access the env variable in nodejs. Value of : process.env" + process.env);
     var DB_INFO = process.env.DB_INFO;
